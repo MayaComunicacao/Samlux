ca0042f9ae850e62be83e36f54ce8479bbe5e1c8
Maya <86801031+MayaComunicacao@users.noreply.github.com>
2023-02-10 18:57:26 -0300

    Update send_mail.ts
    Delete api_key on file

--- src/pages/api/send_mail.ts
+++ src/pages/api/send_mail.ts
@@ -1,9 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import * as SibApivV3Sdk from '@sendinblue/client';
 
-const api_key =
-  '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';
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
