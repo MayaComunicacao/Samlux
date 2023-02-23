fe88a65c51acd482897d99d088ab5aac1c662a80
MayaComunicacao <suporte@mayacomunicacao.com.br>
2023-02-23 11:00:48 -0300

    add meta tags og

--- src/components/layout/Head.tsx
+++ src/components/layout/Head.tsx
@@ -9,9 +9,20 @@ const HeadApp = () => {
         name="description"
         content="Samlux - Iluminação, Decoração, Design e Sofisticação."
       />
+      <meta property="og:title" content="Samlux" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.samlux.com.br" />
+      <meta property="og:image" content="https://samlux.com.br/logo.jpg" />
       <meta
-        property="og:image"
-        content="https://www.samlux.com.br/favicon.jpg"
+        property="og:image:secure_url"
+        content="https://samlux.com.br/logo.jpg"
+      />
+      <meta property="og:image:type" content="image/jpeg" />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" />
+      <meta
+        property="og:image:alt"
+        content="Samlux - Iluminação, Decoração, Design e Sofisticação.t"
       />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
